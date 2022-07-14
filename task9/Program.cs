eb51cccad009e2c43cf97068efe41c0311bb2d73
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-07-14 15:58:07 +0400

    add function

--- task9/Program.cs
+++ task9/Program.cs
@@ -7,18 +7,36 @@
 
 
 int num = new Random().Next(10, 100);
-int firstDigit = num / 10;
-int secondDigit = num % 10;
+// int firstDigit = num / 10;
+// int secondDigit = num % 10;
 
-Console.WriteLine($"Число:  {num} ");
+// Console.WriteLine($"Число:  {num} ");
 
-if(firstDigit == secondDigit)
-{
-    Console.WriteLine("Цифры равны");
+// if(firstDigit == secondDigit)
+// {
+//     Console.WriteLine("Цифры равны");
     
+// }
+//  else if(firstDigit < secondDigit){
+//     Console.WriteLine($"Большее: {secondDigit}");
+// }else{
+//     Console.WriteLine($"Большее: {firstDigit}");
+// }
+
+
+
+int MaxDigit(int number)
+{
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if(firstDigit == secondDigit)return  -1;
+if(firstDigit < secondDigit)return secondDigit;
+return firstDigit;
+ 
 }
- else if(firstDigit < secondDigit){
-    Console.WriteLine($"Большее: {secondDigit}");
-}else{
-    Console.WriteLine($"Большее: {firstDigit}");
-}
+
+
+int maxDigit = MaxDigit(num);
+Console.WriteLine($"Число:  {num} ");
+if(maxDigit == 0) Console.WriteLine($"{num} Цифры одинаковые");
+else Console.WriteLine(maxDigit);
