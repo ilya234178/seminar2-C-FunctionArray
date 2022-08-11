665ef8ba3b84b8e4395827bd9f8fba951d0efdb3
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-08-12 00:25:45 +0400

    add Program

--- task42/Program.cs
+++ task42/Program.cs
@@ -8,56 +8,32 @@
 // 3 -> 11
 // 2 -> 10
 
+Console.Clear();
 int num = GetUserInput();
 
-Console.Write(ConvertToBinary(num));
-Console.WriteLine();
+System.Console.WriteLine(ConvertToBinary(num));
 
 string ConvertToBinary(int num)
 {
-    string binary = string.Empty;
-
-    while (num >= 1)
-    {
-        binary += num % 2;
-        num /= 2;
-    }
-    var item = binary.Reverse();
-    binary = "";
-    foreach (var i in item)
-    {
-        binary += i.ToString();
-    }
-    return binary;
-}
-string WriteArrayAsString(int[] array)
+string binary = string.Empty;
+while (num >= 1)
 {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < array.Length; i++)
-    {
-        sb.Append(array[i]);
-    }
-    return sb.ToString();
+binary+= num % 2;
+num /= 2;
 }
-void Reverse(int[] arr)
+var item = binary.Reverse();
+binary = "";
+foreach(var i in item)
 {
-    int size = arr.Length;
-    int index1 = 0;
-    int index2 = size - 1;
+binary += i.ToString();
+}
 
-    while (index1 < index2)
-    {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
+return binary;
 
-        index1++;
-        index2--;
-    }
 }
 
 int GetUserInput()
 {
-    Console.WriteLine("Введите число: ");
-    return Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+return Convert.ToInt32(Console.ReadLine());
 }
