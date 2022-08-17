47b5e50ecbf4f08a88058ef4e4a9adbdd835dabd
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-08-17 16:49:44 +0400

    add Program

--- task55/Program.cs
+++ task55/Program.cs
@@ -16,7 +16,9 @@ int[,] GetMatrix(int m, int n)
 
         }
     }
-    return matrix;
+     return matrix;
+
+
 }
 void PrintMatrix(int[,] array)
 {
@@ -24,14 +26,15 @@ void PrintMatrix(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            if (j== 0) Console.Write("[");
+            if (j == 0) Console.Write("[");
             if (j < array.GetLength(1) - 1) Console.Write($"{array[j, i],3}, ");
             else Console.Write($"{array[j, i],3}]");
         }
         Console.WriteLine();
     }
+
 }
-void SecondMatrix(int[,]array)
+void SecondMatrix(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -39,13 +42,20 @@ void SecondMatrix(int[,]array)
         {
             if (j == 0) Console.Write("[");
             if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
-            else Console.Write($"{array[i, j],3}]"); 
+            else Console.Write($"{array[i, j],3}]");
         }
-    Console.WriteLine();
+        Console.WriteLine();
     }
+
 }
 
-int[,] matrixResult = GetMatrix(4,4);
+int[,] matrixResult = GetMatrix(3, 5);
+if (matrixResult.GetLength(0)> matrixResult.GetLength(1) 
+
+||  matrixResult.GetLength(0) < matrixResult.GetLength(1)) 
+{
+ Console.WriteLine("Нужно равное число строк и столбцов в массиве");
+}else
 PrintMatrix(matrixResult);
 Console.WriteLine();
 SecondMatrix(matrixResult);
