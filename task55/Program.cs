0ac6af210dfa83a17185bc08b2ae23904c06aee8
Ilya Veryuhanov <veruhanov00@mail.ru>
2022-08-17 15:18:56 +0400

    add Program

---
+++ task55/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для
+// пользователя.
+
+Console.Clear();
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j== 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[j, i],3}, ");
+            else Console.Write($"{array[j, i],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+void SecondMatrix(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3}, ");
+            else Console.Write($"{array[i, j],3}]"); 
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] matrixResult = GetMatrix(4,4);
+PrintMatrix(matrixResult);
+Console.WriteLine();
+SecondMatrix(matrixResult);
+
